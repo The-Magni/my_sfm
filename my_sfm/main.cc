03aa75737e5e14f04daced82a0b5af0f0c4b3d03
The-Magni <dinhquanganh1606@gmail.com>
2026-01-07 11:31:10 +0800

    clean code

--- my_sfm/main.cc
+++ my_sfm/main.cc
@@ -7,6 +7,7 @@
 #include "two_view_geometries.h"
 #include "visualization/visualization.h"
 #include <cassert>
+#include <cstring>
 #include <glog/logging.h>
 #include <memory>
 #include <opencv2/core/types.hpp>
@@ -16,12 +17,13 @@ int main(int argc, char *argv[])
 {
     google::InitGoogleLogging(argv[0]);
     FLAGS_logtostderr = true;
-    if (argc != 2) {
+    if (argc != 3) {
         LOG(ERROR) << "Must provide the specific step in sfm and the config file path";
-       return 1;
+        google::ShutdownGoogleLogging();
+        return 1;
     }
     YAMLConfig yaml_config;
-    yaml_config.Init(argv[1]);
+    yaml_config.Init(argv[2]);
     std::string database_path = yaml_config.Get<std::string>("database_path");
     std::string data_path = yaml_config.Get<std::string>("data_path");
     std::shared_ptr<Images> imgs(new Images(yaml_config.Get<std::string>("images_path")));
@@ -39,16 +41,27 @@ int main(int argc, char *argv[])
     Reconstruction recon;
     Visualization viz;
 
-
     da.Init(imgs, keypoints_db, descriptors_db, two_view_db);
-    da.Process();
-
-    recon.Init(two_view_db, imgs, keypoints_db, outlier_filtering);
-    recon.IncrementalReconstruction();
-    recon.Write(data_path);
-
     viz.Init(data_path);
-    viz.Process();
+
+
+    if (strcmp(argv[1], "data_association") == 0)
+        da.Process();
+    else if(strcmp(argv[1], "reconstruction") == 0) {
+        recon.Init(two_view_db, imgs, keypoints_db, outlier_filtering);
+        recon.IncrementalReconstruction();
+        recon.Write(data_path);
+    } else if (strcmp(argv[1], "viz") == 0)
+        viz.Process();
+    else if (strcmp(argv[1], "full_reconstruction") == 0) {
+        da.Process();
+        recon.Init(two_view_db, imgs, keypoints_db, outlier_filtering);
+        recon.IncrementalReconstruction();
+        recon.Write(data_path);
+        viz.Process();
+    } else {
+        LOG(ERROR) << "Options not available" << '\n';
+    }
 
     google::ShutdownGoogleLogging();
     return 0;
